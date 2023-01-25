1500e4013de4938441d62abb4651ca34d32ee707
Russ Sobti <russ@recombinantsoftware.com>
2023-01-24 16:04:04 -0800

    lab 01 sources
    poggies

---
+++ Lab 01/SOP_fullAdder.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/24/2023 02:47:40 PM
+// Design Name: 
+// Module Name: SOP_fullAdder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SOP_fullAdder(
+    input a,
+    input b,
+    input c,
+    input sum,
+    output carry
+    );
+endmodule
